fcf13bde00c9d0f165d3cf3ac8c1c4612b9c1d03
ans4asif <ans4asif@gitlab.com>
2022-03-29 00:42:35 +0500

    tags component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Tags from './components/Tags';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tags variant="primary" />
+      <Tags variant="secondary" />
+      <Tags variant="tertiary" />
+      <Tags variant="warning" />
+      <Tags variant="default" />
+
+      <div className="App">
+        <Tags variant="primary" isRemovable />
+        <Tags variant="secondary" isRemovable />
+        <Tags variant="tertiary" isRemovable />
+        <Tags variant="warning" isRemovable />
+        <Tags variant="default" isRemovable />
+      </div>
     </div>
   );
 }
---
+++ src/components/Tags/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { TagInput } from 'evergreen-ui';
+import { css } from 'styled-components';
+interface TagsProps {
+  values?: string[];
+  onChange?: (tags: string[]) => void;
+  inputProps?: object;
+  variant?:
+    | 'default'
+    | 'primary'
+    | 'success'
+    | 'warning'
+    | 'secondary'
+    | 'danger'
+    | 'info'
+    | 'tertiary';
+  sm?: boolean;
+  isRemovable?: boolean;
+}
+const Tags = ({
+  values,
+  onChange,
+  inputProps,
+  variant,
+  sm,
+  isRemovable,
+}: TagsProps): JSX.Element => {
+  const [valuez, setValues] = useState(['Label']);
+  const sharedProps = {
+    color:
+      variant === 'default'
+        ? '#021434'
+        : variant === 'secondary'
+        ? '#940FFB'
+        : variant === 'primary'
+        ? '#00B53F'
+        : variant === 'warning'
+        ? '#FF4E35'
+        : variant === 'tertiary'
+        ? '#00B0F0'
+        : '#17a2b8',
+    background:
+      variant === 'default'
+        ? '#021434'
+        : variant === 'tertiary'
+        ? '#00B0F0'
+        : variant === 'primary'
+        ? '#00B53F'
+        : variant === 'secondary'
+        ? '##940FFB'
+        : variant === 'warning'
+        ? '#FF4E35'
+        : '#17a2b8',
+    height: sm ? '25px' : '32px',
+    fontSize: sm ? '14px' : '16px',
+    paddingX: sm ? '15px' : '20px',
+    paddingRight: sm ? '15px' : '20px',
+    borderRadius: '25px',
+    fontWeight: 'bold',
+    isRemovable: isRemovable,
+  };
+  return (
+    <TagInput
+      inputProps={{
+        border: 'none !important',
+        outline: 'none',
+        display: 'none',
+        //remove input border
+      }}
+      values={valuez}
+      tagProps={sharedProps}
+      onChange={(tags: string[]) => {
+        setValues(tags);
+      }}
+    />
+  );
+};
+
+export default Tags;
